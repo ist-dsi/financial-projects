1c9197a9680f97b11bef354708586475ee0c8ece
Luis Cruz <luis.cruz@ist.utl.pt>
2014-05-23 15:55:47 +0100

    Added even more authorizations to view project report stuff.

--- financial-projects-viewer/src/main/java/module/projects/presentationTier/vaadin/reportType/ProjectReportType.java
+++ financial-projects-viewer/src/main/java/module/projects/presentationTier/vaadin/reportType/ProjectReportType.java
@@ -2,10 +2,12 @@ package module.projects.presentationTier.vaadin.reportType;
 
 import java.util.Map;
 
+import module.projects.domain.AccessControl;
 import module.projects.presentationTier.vaadin.IllegalAccessException;
 import module.projects.presentationTier.vaadin.reportType.components.ProjectHeaderComponent;
 import pt.ist.bennu.core.applicationTier.Authenticate.UserView;
 import pt.ist.bennu.core.domain.RoleType;
+import pt.ist.bennu.core.domain.User;
 import pt.ist.expenditureTrackingSystem.domain.organization.Person;
 import pt.ist.expenditureTrackingSystem.domain.organization.Project;
 import pt.ist.expenditureTrackingSystem.domain.organization.SubProject;
@@ -47,10 +49,9 @@ public abstract class ProjectReportType extends ReportType {
     }
 
     private boolean checkAccessControl(String unit) {
-        Unit project = FenixFramework.getDomainObject(unit);
-        Person currentUser = UserView.getCurrentUser().getExpenditurePerson();
-        return project.isResponsible(currentUser) || project.getObserversSet().contains(currentUser)
-                || currentUser.getUser().hasRoleType(RoleType.MANAGER);
+        final Unit project = FenixFramework.getDomainObject(unit);
+        final User user = UserView.getCurrentUser();
+        return AccessControl.isUserAllowedToViewDetailedProjectInfo(project, user);
     }
 
     private Project getProjectFromID(String projectID) {
